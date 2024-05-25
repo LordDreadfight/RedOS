c3261a21a0c46bb116cd1a94e5f52d8c71abb1d6
Beta <119334716+LordDreadfight@users.noreply.github.com>
2024-05-25 12:08:29 +0200

    myOS first steps in kernel

---
+++ build.sh
@@ -0,0 +1,7 @@
+nasm -f elf32 kernel.asm -o kasm.o
+gcc -m32 -c kernel.c -o kc.o
+ld -m elf_i386 -T link.ld -o build/boot/kernel.bin kasm.o kc.o
+qemu-system-i386 -kernel build/boot/kernel.bin
+grub-mkrescue -o RedOS.iso build/
+
+read a
